ef6ba044e73fb71f9a541cf412b57a5535993603
Jack Crawford <jack.crawford@trademe.co.nz>
2025-10-06 07:53:18 +1300

    config for deploy

--- client/src/api/purchase.ts
+++ client/src/api/purchase.ts
@@ -1,6 +1,38 @@
+// import { HostedPurchaseRequest, HostedPurchaseResponse } from "../types/types";
+
+// const LOCAL_PROXY_URL = "http://localhost:3000/api/proxy/purchase";
+
+// type ClientPurchaseRequest = Omit<
+//   HostedPurchaseRequest,
+//   "paystation_id" | "gateway_id"
+// >;
+
+// export async function requestHostedPurchase(
+//   data: ClientPurchaseRequest
+// ): Promise<HostedPurchaseResponse> {
+//   try {
+//     const response = await fetch(LOCAL_PROXY_URL, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error("Failed to create hosted purchase");
+//     }
+
+//     return await response.json();
+//   } catch (error) {
+//     throw new Error("Failed to create hosted purchase");
+//   }
+// }
+
 import { HostedPurchaseRequest, HostedPurchaseResponse } from "../types/types";
 
-const LOCAL_PROXY_URL = "http://localhost:3000/api/proxy/purchase";
+const BASE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const PROXY_URL = BASE_API_URL + "/api/proxy/purchase";
 
 type ClientPurchaseRequest = Omit<
   HostedPurchaseRequest,
@@ -11,7 +43,7 @@ export async function requestHostedPurchase(
   data: ClientPurchaseRequest
 ): Promise<HostedPurchaseResponse> {
   try {
-    const response = await fetch(LOCAL_PROXY_URL, {
+    const response = await fetch(PROXY_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
